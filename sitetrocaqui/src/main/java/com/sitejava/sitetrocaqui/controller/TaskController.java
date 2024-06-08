b6dc46f5d5e3a454b5ce3ff1f40475559c6c3b94
Mattgasan <mathgabriel@outlook.com.br>
2024-06-08 02:17:18 -0300

    Fiz mudanças

--- sitetrocaqui/src/main/java/com/sitejava/sitetrocaqui/controller/TaskController.java
+++ sitetrocaqui/src/main/java/com/sitejava/sitetrocaqui/controller/TaskController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import com.sitejava.sitetrocaqui.model.Task;
 
 @Controller
