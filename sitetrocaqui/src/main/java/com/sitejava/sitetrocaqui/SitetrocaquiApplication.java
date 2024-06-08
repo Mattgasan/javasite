ede658f6757e71d1cceeb28e7d2463b6ea7759c1
Matheus Gasan <112701507+Mattgasan@users.noreply.github.com>
2024-06-08 01:07:44 -0300

    upload

---
+++ sitetrocaqui/src/main/java/com/sitejava/sitetrocaqui/SitetrocaquiApplication.java
@@ -0,0 +1,13 @@
+package com.sitejava.sitetrocaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitetrocaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitetrocaquiApplication.class, args);
+	}
+
+}
